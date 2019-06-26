2329ea823f8ef4bfaed3ee5fd287a37642fb274e
Pedro <motilagem@gmail.com>
2019-06-26 12:33:21 -0300

    ListModel

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -7,45 +7,36 @@ class NegociacaoController {
        this._inputData = $('#data');
        this._inputQuantidade = $('#quantidade');
        this._inputValor = $('#valor');
-
+       this._listaNegociacoes = new ListaNegociacoes();
     }
 
     adiciona(event) {
         
         event.preventDefault();
 
-        let negociacao = new Negociacao(
+        this._listaNegociacoes.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+
+
+        console.log(this._listaNegociacoes.negociacoes);    
+
+    }
+
+    _criaNegociacao() {
+
+        return new Negociacao(
             DateHelper.textoParaData(this._inputData.value),
             this._inputQuantidade.value,
-            this._inputValor.value
-        );
+            this._inputValor.value);
+    }
 
-        
-        
-        console.log(negociacao);    
-        console.log(DateHelper.dataParaTexto(negociacao.data));
+    _limpaFormulario() {
 
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0.0;
+
+        this._inputData.focus();
     }
 
 } // Fim NegociacaoController
-
-
-class PessoaController {
-
-    constructor() {
-
-        this._nome = prompt('Digite seu primeiro nome');
-        this._sobrenome = prompt('Digite seu segundo nome');
-    }
-
-    click(event){
-
-        event.preventDefault();
-
-        let pessoa = new Pessoa(
-            this._nome,
-            this._sobrenome
-        );
-        console.log(pessoa);
-    }
-}
